ebc2da222cc6fdee4f336b852d7d0b518bf48412
Stefan Eggenschwiler <1685438+StefanEggenschwiler@users.noreply.github.com>
2021-10-08 13:42:08 +0200

    Upgraded gradle versions of Labs and fixed Lab04_Android_Toolbar_Apartments

--- 01_labs/Lab04_Android_Toolbar_Apartments/app/src/main/java/ch/hevs/apartments/About.java
+++ 01_labs/Lab04_Android_Toolbar_Apartments/app/src/main/java/ch/hevs/apartments/About.java
@@ -1,15 +1,14 @@
 package ch.hevs.apartments;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.MenuItem;
-import android.widget.Toast;
-import android.support.v4.app.NavUtils;
+
+import androidx.core.app.NavUtils;
 
 public class About extends AppCompatActivity {
 	private String address;
